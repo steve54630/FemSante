6a0058d016b3d07dc47922d7809e38d11bdca0fb
steve5488 <steve5488@hotmail.fr>
2025-11-04 18:41:09 +0100

    18:00 - 04/11 - Ajout de la base Room

---
+++ appli/app/src/main/java/com/audreyRetournayDiet/femSante/env_variables.kt
@@ -0,0 +1,14 @@
+package com.audreyRetournayDiet.femSante
+
+const val PAYPAL_CLIENT_ID =
+    "AVFS5215c8-EqISk9p4xBxKZrzQVUHPLlX9gVgv2VSJmkCKvTJjM4ZV-FORVmJxLjNqvmCQPGF3zBaAt"
+
+const val API_URL = "https://audreyretournay-dieteticiennenutritionniste.fr/api"
+
+const val RETURN_URL_CARD =
+    "com.audreyretournaydiet.femsante.cardpaiement://return_url"
+
+const val RETURN_URL_PAYPAL =
+    "com.audreyRetournayDiet.femSante://paypalpay"
+
+const val AUTHORIZATION_HEADERS = "WGSOGWVVHETODPDS"
